5eb651a91e0a090673f57d6ca3a5c612ebf93b14
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-01-27 14:29:03 -0500

    Modified Hello

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+int x=5;//Added X. This lead to some specific lines in the .s file generated
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    printf("%i!\n",x);//Printing the value of X
+    return 0;// When I am running the -02 flag, i see a lot of comments
+    //generated in the command line but no difference in the output. Maybe i am
+    //using the -02 option wrong?
 }
