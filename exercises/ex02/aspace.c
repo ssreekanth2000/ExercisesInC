ec76f2d57acb2aa890d3adc4237f282d4650e934
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-02-07 10:29:10 -0500

    homework 2 done

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -9,18 +9,33 @@ License: GNU GPLv3
 #include <stdlib.h>
 
 int var1;
+// int check_heap(int var2){
+//   int address_1=&var2;
+//
+//
+// }
 
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *p1 = malloc(128);
+    void *p2 = malloc(16);
+    void *p3 = malloc(16);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
+    printf ("p1 points to %p\n", p1);
+    printf ("p2 points to %p\n", p2);
+    printf ("p3 points to %p\n", p3);
     printf ("s points to %p\n", s);
 
     return 0;
 }
+//output
+// p2 points to 0x1304130
+// p3 points to 0x1304150
+//The difference is 32 when calculated which makes sense as 16_16 is 32.
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -11,17 +11,20 @@ License: GNU GPLv3
 #define SIZE 5
 
 int *foo() {
-    int i;
+    int i;//When I compile this code, it warns me saying that the function foo
+    //retruns the address of a local variable
     int array[SIZE];
 
-    //printf("%p\n", array);
+    //printf("%p\n", array); When I comment the print, it removes that error
+    // but gives me an error for the return array about returning address of
+    //local variable.
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
     }
     return array;
 }
-
+//I get a segementation fault when i try to run it.
 void bar() {
     int i;
     int array[SIZE];
