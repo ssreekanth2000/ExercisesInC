50afbdb5f5627d7c5eefe7d94d3308837af6fb3c
ssreekanth2000 <s.sreekanth2000@gmail.com>
2019-04-08 17:13:38 -0400

    donw?

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -4,23 +4,24 @@
 
 
 
-void print_num(gchar *word, GHashTable *directory){
-  printf("%s:  %ld\n", word, (gint32)g_hash_table_lookup(dictionary, word));
+void print_num(gchar *word, GHashTable *dictionary_of_words){
+  printf("%s:  %ld\n", word, (gint32)g_hash_table_lookup(dictionary_of_words, word));
 }
 
-void frequency_histogram(FILE *input_file, gchar *buffer, gchar *valid_chars,gchar substitute_char, GHashTable *dictionary){
-  while(fscanf(input_file, "%1023s",buffer)==1){
-    g_strcanon(buffer, valid_chars, substitute_char);
-    if(g_strcmp0(buffer," ")!=0 && g_strcmp0(buffer, "") != 0){
-      if(g_hash_table_contains(dictionary, buffer)){
+
+void frequency_histogram(FILE *input_file, gchar *temp, gchar *valid_chars,gchar substitute_char, GHashTable *dictionary){
+  while(fscanf(input_file, "%1023s",temp)==1){
+    g_strcanon(temp, valid_chars, substitute_char);
+    if(g_strcmp0(temp," ")!=0 && g_strcmp0(temp, "") != 0){
+      if(g_hash_table_contains(dictionary, temp)){
         g_hash_table_replace(
-          dictionary, g_strdup(buffer),
-          (gpointer)(((gint64)g_hash_table_lookup(dictionary, buffer))+1));
+          dictionary, g_strdup(temp),
+          (gpointer)(((gint64)g_hash_table_lookup(dictionary, temp))+1));
 
 
       }
      else {
-       g_hash_table_insert(dictionary, g_strdup(buffer),(gpointer)1);
+       g_hash_table_insert(dictionary, g_strdup(temp),(gpointer)1);
      }
 
 
@@ -28,29 +29,33 @@ void frequency_histogram(FILE *input_file, gchar *buffer, gchar *valid_chars,gch
   }
 }
 
+gint compare_numbers(gchar *word1, gchar *word2, GHashTable *dictionary) {
+  return ((gint64)g_hash_table_lookup(dictionary, word1)) -
+         ((gint64)g_hash_table_lookup(dictionary, word2));
+}
 
 int main(int argc, char **argv) {
   if (argc < 2) {
-    puts("Must provide a file name.");
+    puts("no file");
     exit(1);
   }
 
   FILE *text = fopen(argv[1], "r");
   if (!text) {
-    printf("Invalid file.");
+    printf("file not valid.");
     exit(1);
   }
 
   gchar buffer[1024];
-  gchar valid_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  gchar valid[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   gchar substitute_char = 0;
   GHashTable *dictionary = g_hash_table_new(g_str_hash, g_str_equal);
 
-  word_histogram(text, buffer, valid_chars, substitute_char, dictionary);
+  frequency_histogram(text, buffer, valid, substitute_char, dictionary);
 
   g_list_foreach(g_list_sort_with_data(g_hash_table_get_keys(dictionary),
-                                       (GCompareFunc)compare_keys, dictionary),
-                 (GFunc)print_entry, dictionary);
+                                       (GCompareFunc)compare_numbers, dictionary),
+                 (GFunc)print_num, dictionary);
 
   fclose(text);
   exit(0);
